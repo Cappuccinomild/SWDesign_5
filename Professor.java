f2843808b98d9c2ffb22c69916b79025f24d95f9
kyoungmo-park <56715068+kyoungmo-park@users.noreply.github.com>
2019-12-10 17:09:54 +0900

    Add files via upload

---
+++ Professor.java
@@ -0,0 +1,20 @@
+public class Professor {
+	private String ProfessorID, Lecture;
+
+	public String getProfessorID() {
+		return ProfessorID;
+	}
+
+	public void setProfessorID(String professorID) {
+		ProfessorID = professorID;
+	}
+
+	public String getLecture() {
+		return Lecture;
+	}
+
+	public void setLecture(String lecture) {
+		Lecture = lecture;
+	}
+	
+}
